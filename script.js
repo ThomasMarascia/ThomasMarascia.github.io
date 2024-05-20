e1cc578156e4abb9b973982c31e4b98ec83162a8
Thomas Marascia <Thomasmarascia@gmail.com>
2024-05-20 01:25:02 -0500

    bitmogi, resume, and scripts added

---
+++ script.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const elements = document.querySelectorAll(".typewriter-text");
+    const speed = 100;
+    const pauseBetweenElements = 300;
+
+    function typeWriter(element, text, index, callback) {
+        if (index < text.length) {
+            element.textContent += text.charAt(index);
+            setTimeout(() => typeWriter(element, text, index + 1, callback), speed);
+        } else {
+            callback();
+        }
+    }
+  
+    function startTypewriterEffect(elements, index = 0) {
+        if (index < elements.length) {
+            const element = elements[index];
+            const text = element.getAttribute('data-text');
+            typeWriter(element, text, 0, () => {
+                setTimeout(() => startTypewriterEffect(elements, index + 1), pauseBetweenElements);
+            });
+        }
+    }
+  
+    startTypewriterEffect(elements);
+  });
