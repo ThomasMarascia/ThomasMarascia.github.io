fae38d9875308b3960e0d2300142d68f6439910d
Thomas Marascia <Thomasmarascia@gmail.com>
2024-05-21 22:07:29 -0500

    add privacy page, added some scripts

--- scripts/script.js
+++ scripts/script.js
@@ -24,3 +24,17 @@ document.addEventListener("DOMContentLoaded", function() {
   
     startTypewriterEffect(elements);
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const navLinks = document.querySelectorAll('.navbar-nav .nav-link');
+
+    navLinks.forEach(function(navLink) {
+        navLink.addEventListener('click', function() {
+            navLinks.forEach(function(link) {
+                link.classList.remove('active');
+            });
+
+            this.classList.add('active');
+        });
+    });
+});
